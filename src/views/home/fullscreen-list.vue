85d805d9869774629c0536bcaa0932b3941ce07a
chenshuideshizi <fanwenxiang326@163.com>
2023-10-21 22:02:37 +0800

    修改swiper列表

--- src/views/home/fullscreen-list.vue
+++ src/views/home/fullscreen-list.vue
@@ -7,31 +7,24 @@
       <div class="swiper-slide">Slide 2</div>
       <div class="swiper-slide">Slide 3</div>
     </div>
-    <!-- If we need pagination -->
+
     <div class="swiper-pagination"></div>
 
-    <!-- If we need navigation buttons -->
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
-
-    <!-- If we need scrollbar -->
-    <div class="swiper-scrollbar"></div>
   </div>
 
 </template>
 
 <script setup>
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { onMounted } from 'vue';
 
 onMounted(() => {
   const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination],
+    modules: [ Pagination],
     direction: 'vertical',
     loop: true,
 
@@ -39,17 +32,6 @@ onMounted(() => {
       el: '.swiper-pagination',
       type: "fraction",
     },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
   });
 })
 
